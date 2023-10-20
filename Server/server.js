f48732a4db810efb394618d047ad5e02d8cfe6c2
Chaiyarin Niamsuwan <chaiyarin.aof@gmail.com>
2023-10-20 12:25:28 +0700

    ทำให้ส่งข้อมูลระหว่่างกันได้แล้ว

---
+++ Server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+// Use CORS middleware
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('send-hello', (data) => {
+        console.log(data);
+        io.emit('receive-hello', { data: data });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
